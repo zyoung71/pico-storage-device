a92e5e05aaf548c456f5c40faef989a4fc0dbe78
zyoung71 <zyoung0071@gmail.com>
2026-01-07 17:35:24 -0500

    func change

--- src/storage/FileContentsMap.cpp
+++ src/storage/FileContentsMap.cpp
@@ -8,7 +8,7 @@ UniqueArray<char> FileContentsMap::GetValue(const char* key)
     int64_t idx = storage_dev->FindNextString(key);
     if (idx >= 0)
     {
-        storage_dev->SeekStep(idx + strlen(key) + 1);
+        storage_dev->Seek(idx + strlen(key) + 1); // move past key and equals sign
         UniqueArray<char> ret;
         storage_dev->ReadLine(ret, false);
         return std::move(ret);
